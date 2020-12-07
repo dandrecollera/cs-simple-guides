5799f104d86071f2d064971d3b04680fcd5fd517
dandrecollera <52880769+dandrecollera@users.noreply.github.com>
2020-12-07 11:37:10 +0800

    Create simple-main-yn.cs

---
+++ simple-main-yn/simple-main-yn.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace randomz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char decision = 'Z'; //Set Default to Z
+            while(decision != 'N')
+            {
+                //code here
+
+                Console.Write("Would you like to try again? Y/N: ");
+                decision = Char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                if(decision == 'Y')
+                {
+                    Console.WriteLine("Trying Again.");
+                    continue;
+                }
+                else if(decision == 'N')
+                {
+                    decision = 'N';
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input");
+                    continue;
+                }
+            }
+        }
+    }
+}
